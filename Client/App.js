d66bc9b205753a7418f584ac83ca8303398ce054
sujal <sujalpatel2753@gmail.com>
2024-01-31 17:01:12 +0530

    allow

--- Client/App.js
+++ Client/App.js
@@ -55,7 +55,7 @@ const App = () => {
   useEffect(async()=>{
     const getData=()=>{
         console.log("hiiiii byeeeeeee");
-        console.log("why checkkkkggg");
+        console.log("why checkkkkggg mainnn");
     }
 
    
